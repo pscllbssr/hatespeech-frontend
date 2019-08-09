785bacc21e0f287c6755937253cdb8fb91267870
Pascal <pa@catatec.ch>
2019-08-09 17:20:08 +0200

    latest build

---
+++ docs/.vuepress/dist/assets/js/7.ef4353b6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{128:function(t,e,n){"use strict";n.r(e);var i={mounted:function(){var t=this.$site.themeConfig.api,e=new FormData;e.append("text","dummy"),fetch(t,{method:"POST",body:e})}},o=n(10),a=Object(o.a)(i,function(){var t=this.$createElement;return(this._self._c||t)("div")},[],!1,null,null,null);e.default=a.exports}}]);
