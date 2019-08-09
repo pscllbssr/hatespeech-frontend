9e1e59be6e78447f0dafee839d893229b87c5eec
Pascal <pa@catatec.ch>
2019-08-09 17:14:22 +0200

    remove unused text, wake up call for api

---
+++ docs/.vuepress/components/WakeUpApi.vue
@@ -0,0 +1,19 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    mounted: function(){
+        const api_location = this.$site.themeConfig.api
+
+        let formData = new FormData();
+        formData.append('text', 'dummy');
+
+        fetch(api_location, {
+                    method: 'POST', 
+                    body: formData                  
+                })
+    }
+}
+</script>
