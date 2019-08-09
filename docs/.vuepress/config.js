10297f2088668d7fd191bada69b7b00fb665ae6e
Pascal <pa@catatec.ch>
2019-08-09 17:16:08 +0200

    add open graph meta tags

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -32,8 +32,20 @@ module.exports = {
             href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
           }
         ],
-          ['link', { rel: 'icon', href: '/favicon.ico' }]
+        ['link', { rel: 'icon', type: 'image/png', sizes:"96x96", href: '/favicon-96x96.png' }],
+        ['link', { rel: 'icon', type: 'image/png', sizes:"32x32", href: '/favicon-32x32.png' }],
+        ['link', { rel: 'icon', type: 'image/png', sizes:"16x16", href: '/favicon-16x16.png' }],
+
+        ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
+
         
-      ]
+        // og
+        ['meta', {property: 'og:image', content: 'https://projekte.lbssr.ch/der-hass-im-netz/der-hass-im-netz.jpg'}],
+        ['meta', {property: 'og:title', content: 'Der Hass im Netz'}],
+        ['meta', {property: 'og:description', content: 'Hasskommentare im Internet sind ein zunehmendes Problem. Viele hoffen deshalb auf eine künstliche Intelligenz. Doch können Maschinen für uns das Internet retten?'}],
+        ['meta', {property: 'og:url', content: 'https://projekte.lbssr.ch/der-hass-im-netz/'}],
+
+      ],
+    //dest: '.vuepress/deste'
 
 }
